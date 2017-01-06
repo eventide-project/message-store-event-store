69d7f924cc8849a663ab622400d5fc6773f29740
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-05 18:31:34 -0600

    Session log tags are reworked

--- lib/event_source/event_store/http/session.rb
+++ lib/event_source/event_store/http/session.rb
@@ -29,13 +29,13 @@ module EventSource
         def call(request, redirect: nil, &probe)
           redirect ||= false
 
-          logger.trace(tags: :http) { "Issuing #{request.method} request (#{LogText.request_attributes request})" }
+          logger.trace(tag: :event_store_http) { "Issuing #{request.method} request (#{LogText.request_attributes request})" }
           logger.trace(tag: :data) { LogText.request_body request } if request.request_body_permitted?
 
           response = connection.request request
 
           if Net::HTTPRedirection === response && !redirect
-            logger.debug(tags: :http) { "#{request.method} request received redirect response (#{LogText.request_attributes request}, #{LogText.response_attributes response}, Location: #{response['Location'] || '(none)'})" }
+            logger.debug(tag: :event_store_http) { "#{request.method} request received redirect response (#{LogText.request_attributes request}, #{LogText.response_attributes response}, Location: #{response['Location'] || '(none)'})" }
 
             location = URI.parse response['Location']
             leader_ip_address = location.host
@@ -50,7 +50,7 @@ module EventSource
             return response
           end
 
-          logger.trace(tags: :http) { "#{request.method} request issued (#{LogText.request_attributes request}, #{LogText.response_attributes response})" }
+          logger.trace(tag: :event_store_http) { "#{request.method} request issued (#{LogText.request_attributes request}, #{LogText.response_attributes response})" }
           logger.trace(tag: :data) { LogText.response_body response }
 
           telemetry.record :http_request, Telemetry::HTTPRequest.build(request, response)
@@ -75,7 +75,7 @@ module EventSource
         end
 
         def establish_connection(leader_ip_address=nil)
-          logger.trace { "Establishing connection to EventStore (#{LogText.establishing_connection self, leader_ip_address})" }
+          logger.trace(tag: :event_store_connection) { "Establishing connection to EventStore (#{LogText.establishing_connection self, leader_ip_address})" }
 
           close if connected?
 
@@ -90,7 +90,7 @@ module EventSource
 
             rescue ::EventStore::Cluster::LeaderStatus::GossipEndpoint::Get::NonClusterError => error
               leader_status_queried_telemetry.error = error
-              logger.warn { "Could not determine cluster leader (#{LogText.establishing_connection self, leader_ip_address}, Error: #{error.class})" }
+              logger.warn(tag: :event_store_connection) { "Could not determine cluster leader (#{LogText.establishing_connection self, leader_ip_address}, Error: #{error.class})" }
             end
 
             telemetry.record :leader_status_queried, leader_status_queried_telemetry
@@ -100,7 +100,7 @@ module EventSource
 
           telemetry.record :connection_established, Telemetry::ConnectionEstablished.new(leader_ip_address || host, port, connection)
 
-          logger.debug { "Connection to EventStore established (#{LogText.connection_established self, leader_ip_address})" }
+          logger.info(tag: :event_store_connection) { "Connection to EventStore established (#{LogText.connection_established self, leader_ip_address})" }
 
           self.connection = connection
 
