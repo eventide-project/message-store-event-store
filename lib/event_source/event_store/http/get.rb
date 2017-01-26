6c74357f3198ec7849aaa36d121118164287f7a2
Nathan Ladd <nathan@obsidianexchange.com>
2017-01-26 10:16:02 -0600

    Get and Read no longer read in reverse

--- lib/event_source/event_store/http/get.rb
+++ lib/event_source/event_store/http/get.rb
@@ -6,17 +6,13 @@ module EventSource
 
         configure :get
 
-        initializer :batch_size, w(:precedence), a(:long_poll_duration)
-
-        def precedence
-          @precedence ||= Defaults.precedence
-        end
+        initializer :batch_size, a(:long_poll_duration)
 
         dependency :read_stream, ::EventStore::HTTP::ReadStream
         dependency :session, Session
 
-        def self.build(batch_size: nil, precedence: nil, long_poll_duration: nil, session: nil)
-          instance = new batch_size, precedence, long_poll_duration
+        def self.build(batch_size: nil, long_poll_duration: nil, session: nil)
+          instance = new batch_size, long_poll_duration
 
           session = Session.configure instance, session: session
           ::EventStore::HTTP::ReadStream.configure instance, session: session
@@ -42,66 +38,21 @@ module EventSource
         def call(stream_name, position: nil)
           stream_name = StreamName.canonize stream_name
 
-          logger.trace { "Reading stream (StreamName: #{stream_name}, Position: #{position || '(start)'}, Direction: #{direction}, BatchSize: #{batch_size})" }
-
-          if precedence == :desc
-            position = desc_position stream_name, position
-
-            if position < 0
-              logger.debug { "Reading backward across start of stream (StreamName: #{stream_name}, Position: #{position || '(start)'}, Direction: #{direction}, BatchSize: #{batch_size}, Events: 0)" }
-              return []
-            end
-          end
+          logger.trace { "Reading stream (StreamName: #{stream_name}, Position: #{position || '(start)'}, BatchSize: #{batch_size})" }
 
           begin
             events = read_stream.(
               stream_name,
               position: position,
-              direction: direction,
               batch_size: batch_size
             )
           rescue ::EventStore::HTTP::ReadStream::StreamNotFoundError
             events = []
           end
 
-          events.reverse! if precedence == :desc
-
-          logger.debug { "Done reading stream (StreamName: #{stream_name}, Position: #{position || '(start)'}, Direction: #{direction}, BatchSize: #{batch_size}, Events: #{events.count})" }
+          logger.debug { "Done reading stream (StreamName: #{stream_name}, Position: #{position || '(start)'}, BatchSize: #{batch_size}, Events: #{events.count})" }
 
           events
-        end
-
-        def desc_position(stream_name, position)
-          begin
-            head_event, * = read_stream.(
-              stream_name,
-              position: :head,
-              direction: :backward,
-              batch_size: 1
-            )
-          rescue ::EventStore::HTTP::ReadStream::StreamNotFoundError
-            return -1
-          end
-
-          if position.nil?
-            head_event.position
-          else
-            head_event.position - position
-          end
-        end
-
-        def direction
-          if precedence == :asc
-            :forward
-          else
-            :backward
-          end
-        end
-
-        module Defaults
-          def self.precedence
-            :asc
-          end
         end
       end
     end
--- lib/event_source/event_store/http/read.rb
+++ lib/event_source/event_store/http/read.rb
@@ -4,31 +4,8 @@ module EventSource
       class Read
         include EventSource::Read
 
-        def self.build(*)
-          instance = super
-          instance.configure_long_poll
-          instance
-        end
-
-        def configure(batch_size: nil, precedence: nil, session: nil)
-          Get.configure(
-            self,
-            batch_size: batch_size,
-            precedence: precedence,
-            session: session
-          )
-        end
-
-        def configure_long_poll
-          cycle = iterator.cycle
-
-          return if cycle.instance_of? Cycle::None
-
-          maximum_milliseconds = cycle.maximum_milliseconds
-
-          long_poll_duration = Rational(maximum_milliseconds, 1000).ceil
-
-          get.read_stream.enable_long_poll long_poll_duration
+        def configure(batch_size: nil, session: nil)
+          Get.configure self, batch_size: batch_size, session: session
         end
       end
     end
